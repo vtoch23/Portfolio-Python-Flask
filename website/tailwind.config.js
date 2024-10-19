e9d06d7ebf39d39aaf760fb0d9af5b1fcd64ac32
Vanya <vtochevska@gmail.com>
2024-10-19 11:16:18 +0300

    tailwind config

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "src/app/app.component.html",
+    'templates/.{js,ts,jsx,tsx,mdx, html}',
     "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
